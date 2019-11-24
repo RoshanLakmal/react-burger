776c7ad2ab29f74f0e2900e6f66923a597dc7157
Roshan <roshanrox911@gmail.com>
2019-11-24 19:52:11 +1100

    44. Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     person: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ]
-  };
+  });
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'am a React App</h1>
-        <Person
-          name={this.state.person[0].name}
-          age={this.state.person[0].age}
-        />
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}>
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.person[2].name}
-          age={this.state.person[2].age}
-        />
-      </div>
-    );
-  }
-}
-export default App;
+  const [otherState, setOtherState] = useState('some other value');
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian'
+    setPersonsState({
+      person: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  };
+  return (
+    <div className="App">
+      <h1>Hi, I'am a React App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.person[0].name}
+        age={personsState.person[0].age}
+      />
+      <Person
+        name={personsState.person[1].name}
+        age={personsState.person[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.person[2].name}
+        age={personsState.person[2].age}
+      />
+    </div>
+  );
+};
+export default app;
